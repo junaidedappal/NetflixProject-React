243e4388fa5b3af7ad68e3d0f1c456238732eb0b
junaid shukapuram <junaidshukapuram@gmail.com>
2022-05-14 00:55:11 +0530

    netflix ui hard copy

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+      
+
+       
+    </div>
+  );
+}
+
+export default App;
